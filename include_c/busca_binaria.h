cbd81437cecfa9ab924422bcf3fe1f5eac95346d
Paulo Rogério <80416920+RealMeloLima@users.noreply.github.com>
2025-09-22 21:22:31 -0300

    Add files via upload

---
+++ include_c/busca_binaria.h
@@ -0,0 +1,7 @@
+#ifndef BUSCA_BINARIA_H
+#define BUSCA_BINARIA_H
+
+int busca_binaria(int n);
+bool isBadVersion(int version);
+
+#endif
---
+++ include_c/busca_seq_ordenada.h
@@ -0,0 +1,8 @@
+#ifndef BUSCA_SEQ_ORDENADA_H
+#define BUSCA_SEQ_ORDENADA_H
+
+void bubbleSort(int arr[], int n);
+int busca_seq_ordenada(int arr[], int n, int alvo);
+int conta_especialidades_distintas(int arr[], int n);
+
+#endif
